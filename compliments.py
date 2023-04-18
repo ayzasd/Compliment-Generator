998b84248cd80ce8fb0bdf9002d52275334dfbcc
ayzasd <130077458+ayzasd@users.noreply.github.com>
2023-04-18 14:27:52 -0500

    adding exclamations

--- compliments.py
+++ compliments.py
@@ -1,6 +1,6 @@
 import random
 from colorama import Fore
 thing = ["good", "smart", "kind", "caring", "passionate", "intelligent", "good at coding"]
-compliment = "You are" + Fore.BLUE + f" {random.choice(thing)}"
+compliment = "You are" + Fore.BLUE + f" {random.choice(thing)}!"
 def joke():
     return compliment
