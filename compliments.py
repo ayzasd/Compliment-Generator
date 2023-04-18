bdaadf8e161bfedd0a35e481213db51b7b129ffd
ayzasd <130077458+ayzasd@users.noreply.github.com>
2023-04-18 14:24:30 -0500

    making it actually nice!

--- compliments.py
+++ compliments.py
@@ -1,6 +1,6 @@
 import random
 from colorama import Fore
-thing = ["good", "ugly", "smart", "kind", "caring", "dumb", "stupid"]
+thing = ["good", "smart", "kind", "caring", "passionate", "intelligent", "good at coding"]
 compliment = "You are" + Fore.BLUE + f" {random.choice(thing)}"
 def joke():
     return compliment
