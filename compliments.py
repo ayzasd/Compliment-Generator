cb259c9ce1bf4e61baa7ba830cf33f168d548f5f
ayzasd <130077458+ayzasd@users.noreply.github.com>
2023-04-18 14:22:20 -0500

    updating colors

--- compliments.py
+++ compliments.py
@@ -1,6 +1,6 @@
 import random
 from colorama import Fore
 thing = ["good", "ugly", "smart", "kind", "caring", "dumb", "stupid"]
-compliment = "You are" + Fore.GREEN + f"{random.choice(thing)}"
+compliment = "You are" + Fore.BLUE + f" {random.choice(thing)}"
 def joke():
     return compliment
